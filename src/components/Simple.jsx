ec12d9d93d7df201553325679445b6d9a6ee1d98
Marcelo Maia <mmaia.cc@gmail.com>
2019-05-28 15:52:15 -0300

    grafico primeira versao

---
+++ src/components/Simple.jsx
@@ -0,0 +1,179 @@
+import React, { PureComponent } from 'react';
+import EchartsReact from './ReactEcharts';
+
+const option = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      crossStyle: {
+        color: '#999'
+      }
+    }
+  },
+  toolbox: {
+    feature: {
+      dataView: { show: true, readOnly: false },
+      magicType: { show: true, type: ['line', 'bar'] },
+      restore: { show: true },
+      saveAsImage: { show: true }
+    }
+  },
+  legend: {
+    data: ['AAA', 'BBB', 'Temperatura']
+  },
+  xAxis: [
+    {
+      type: 'category',
+      data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+      axisPointer: {
+        type: 'shadow'
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value',
+      name: 'ml/cm³',
+      min: 0,
+      max: 250,
+      interval: 50,
+      axisLabel: {
+        formatter: '{value} ml'
+      }
+    },
+    {
+      type: 'value',
+      name: 'Temp.',
+      min: 0,
+      max: 25,
+      interval: 5,
+      axisLabel: {
+        formatter: '{value} °C'
+      }
+    }
+  ],
+  series: [
+    {
+      name: 'AAA',
+      type: 'line',
+      data: [
+        2.0,
+        4.9,
+        7.0,
+        23.2,
+        25.6,
+        76.7,
+        135.6,
+        162.2,
+        32.6,
+        20.0,
+        6.4,
+        3.3
+      ]
+    },
+    {
+      name: 'BBB',
+      type: 'bar',
+      data: [
+        2.6,
+        5.9,
+        9.0,
+        26.4,
+        28.7,
+        70.7,
+        175.6,
+        182.2,
+        48.7,
+        18.8,
+        6.0,
+        2.3
+      ]
+    },
+    {
+      name: 'Temperatura',
+      type: 'line',
+      yAxisIndex: 1,
+      data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+    }
+  ]
+};
+
+export default class Simple extends PureComponent {
+  getOption = () => {
+    return {
+      title: {
+        text: '堆叠区域图'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['邮件营销', '联盟广告', '视频广告']
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '邮件营销',
+          type: 'line',
+          stack: '总量',
+          areaStyle: { normal: {} },
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: '联盟广告',
+          type: 'line',
+          stack: '总量',
+          areaStyle: { normal: {} },
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: '视频广告',
+          type: 'line',
+          stack: '总量',
+          areaStyle: { normal: {} },
+          data: [150, 232, 201, 154, 190, 330, 410]
+        }
+      ]
+    };
+  };
+  render() {
+    return (
+      <div className="examples">
+        <div className="parent">
+          <label>
+            {' '}
+            render a Simple echart With <strong>option and height</strong>:{' '}
+          </label>
+          <EchartsReact
+            option={option}
+            style={{ height: '350px', width: '100%' }}
+            className="react_for_echarts"
+          />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/utils.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+
+export const pick = (obj, keys) => {
+  const r = {};
+  keys.forEach(key => {
+    r[key] = obj[key];
+  });
+  return r;
+};
