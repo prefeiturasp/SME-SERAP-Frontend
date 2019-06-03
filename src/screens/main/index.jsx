095fbb01f9dd102274d7f8a74ba52ae5214b0113
Marcelo Maia <mmaia.cc@gmail.com>
2019-06-03 18:21:32 -0300

    checkpoint. metade do algumas refatorações de css

--- src/screens/main/index.jsx
+++ src/screens/main/index.jsx
@@ -3,9 +3,8 @@ import Button, { ButtonStyle } from '../../components/Button';
 import CircleAndLabel from '../../components/circle/CircleAndLabel';
 import Grid from '../../components/Grid';
 import './custom.css';
-import byncsa from './img/by-nc-sa.png';
 import livros from './img/livros.png';
-import logoSP from './img/Logo_SP.png';
+import Rodape from './rodape';
 
 export class Main extends Component {
   render() {
@@ -16,14 +15,12 @@ export class Main extends Component {
             <span>Conheça o</span>
             <br />
             <span style={{ color: '#1B80D4' }}>IDEP</span>
-
             <p className="textoPeqCinza">
               Saiba mais sobre a importância do índice, como ele é calculado e
               as metas da sua escola
             </p>
           </div>
         </Grid>
-
         <Grid cols="7 7 7 7" className="col backCustom p-3">
           <Button label="HOME" style={ButtonStyle.OutlineLight} />
           <Button
@@ -62,12 +59,12 @@ export class Main extends Component {
         </Grid>
         {/*  quebra de linha aqui */}
         <div className="w-100" />
-        <Grid cols="5 5 5 5" className="p-5">
-          <p className="textoAzulNegritoItalico ml-3">
+        <Grid cols="5 5 5 5" className="p-3">
+          <p className="textoAzulNegritoItalico">
             Todos os estudantes tem direito não só à educação, mas à educação de
             qualidade.
           </p>
-          <p className="textoPeq2 p-5">
+          <p className="textoPeq2">
             <b>O Índice de Desenvolvimento da Educação (IDEP)</b> foi criado
             para medir o desempenho das escolas da Rede Municipal de Ensino,
             levando em conta os componentes curriculares avaliados na Prova São
@@ -108,94 +105,15 @@ export class Main extends Component {
               bottomLabel="Meta 5,2"
             />
           </Grid>
+          <div>
+            <Button
+              label="Confira mais detalhes na nota técnica"
+              style={ButtonStyle.OutlinePrimary}
+            />
+          </div>
         </Grid>
         <div className="w-100" />
-        <Grid cols="12 12 12 12">
-          <div className="w-100" />
-          <div className="bg-cinza-escuro p-5 text-white area-rodape fonte-doze">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-2 col-xs-12 mb-xs-4 d-flex align-items-end">
-                  <img src={logoSP} alt="Prefeitura de SP" />
-                </div>
-                <div className="col-lg-2 col-xs-12 border-left mb-xs-4">
-                  <ul className=" list-unstyled mb-0">
-                    <li className="text-uppercase font-weight-bold">
-                      Governo Municipal
-                    </li>
-                  </ul>
-                  <ul className="list-unstyled mb-0 mt-0">
-                    <li>Prefeito Bruno Covas</li>
-                    <li>Equipe de Governo</li>
-                    <li>Agenda do Prefeito</li>
-                    <li>Agenda do Secretário</li>
-                  </ul>
-                </div>
-                <div className="col-lg-2 col-xs-12 mb-xs-4">
-                  <ul className="list-unstyled mb-3">
-                    <li className="text-uppercase font-weight-bold">
-                      Mapa do Site
-                    </li>
-                  </ul>
-                  <ul className="list-unstyled mb-1">
-                    <li className="text-uppercase font-weight-bold">
-                      Canais Oficiais
-                    </li>
-                  </ul>
-                  <ul className="list-inline mb-3">
-                    <li className="list-inline-item">
-                      <a className="text-white" href="">
-                        <i className="fab fa-facebook-square" />
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a className="text-white" href="">
-                        <i className="fab fa-instagram" />
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a className="text-white" href="">
-                        <i className="fab fa-twitter-square" />
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a className="text-white" href="">
-                        <i className="fab fa-youtube" />
-                      </a>
-                    </li>
-                  </ul>
-                  <img src={byncsa} alt="Copyright" />
-                </div>
-                <div className="col-lg-4 col-xs-12">
-                  <div className="form-group mb-2">
-                    <select className="form-control fonte-doze rounded-pill text-secondary bg-transparent border border-secondary text-white">
-                      <option>Secretarias</option>
-                    </select>
-                  </div>
-                  <div className="form-group mb-2">
-                    <select className="form-control fonte-doze rounded-pill text-secondary bg-transparent border border-secondary text-white">
-                      <option>Prefeituras Regionais</option>
-                    </select>
-                  </div>
-                  <div className="form-group mb-0">
-                    <select className="form-control fonte-doze rounded-pill text-secondary bg-transparent border border-secondary text-white">
-                      <option>Órgãos e Autarquias</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12 col-xs-12 text-center fonte-doze mt-2 mb-2">
-                SECRETARIA MUNICIPAL DE EDUCAÇÃO — Rua Borges Lagoa, 1230 — Vila
-                Clementino — CEP: 04038-003
-              </div>
-            </div>
-          </div>
-        </Grid>
+        <Rodape />
       </div>
     );
   }
