cc5f04ad1aaaeb779ee801e8801c4d54fbc714e5
Eduardo <eduardo.bufaino@amcom.com.br>
2021-02-04 15:41:24 -0300

    Alteração para migração kuberntes

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Replace string in static files
+# sed -i "s/old-text/new-text/g" input.txt
+
+set -xe
+  : "${SERVER_NAME?Precisa de uma variavel de ambiente SERVER_NAME}"
+
+set -xe
+  : "${REACT_APP_API_URL?Precisa de uma variavel de ambiente REACT_APP_API_URL}"
+
+
+sed -i "s,SERVER_NAME,$SERVER_NAME,g" /etc/nginx/conf.d/default.conf
+
+
+exec "$@"
